2113fa5fd216098e190e89256644891fedf36daa
Jose Cicero <jose.cicero@gmail.com>
2025-12-04 20:20:02 -0300

    correcoes de autorizacoes

--- samba/scripts/start.sh
+++ samba/scripts/start.sh
@@ -2,10 +2,17 @@
 
 apk add --no-cache samba
 adduser -D homelab
-echo -e "1234\n1234" | smbpasswd -a homelab
+
+# define senha Samba corretamente usando printf
+printf "1234\n1234\n" | smbpasswd -a homelab
+
+# habilitar usuário no Samba
+smbpasswd -e homelab
 
 mkdir -p /share/public /share/private
+chown -R homelab:homelab /share/private
 chmod -R 777 /share/public
 chmod -R 770 /share/private
 
+nmbd &
 exec smbd --foreground --no-process-group
