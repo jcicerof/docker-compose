dce09f9f78e406b61650bdb0cc97474d6aff31f5
Jose Cicero <jose.cicero@gmail.com>
2025-12-05 02:11:29 -0300

    samba optmizations

--- samba/scripts/start.sh
+++ samba/scripts/start.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
-apk add --no-cache samba
+apk add --no-cache samba samba-common-tools
 adduser -D homelab
 
 # define senha Samba corretamente usando printf
 printf "1234\n1234\n" | smbpasswd -a homelab
 
-# habilitar usuário no Samba
+# habilitar usuario no Samba
 smbpasswd -e homelab
 
 mkdir -p /share/public /share/private
 chown -R homelab:homelab /share/private
 chmod -R 777 /share/public
 chmod -R 770 /share/private
+
+# Logging
+mkdir -p /var/log/samba
 
 nmbd &
 exec smbd --foreground --no-process-group
